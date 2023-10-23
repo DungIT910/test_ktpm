c022b33c160b59bae2214dc052f06cfbea437a87
DungIT910 <Dungtb611@gmail.com>
2023-10-23 14:04:15 +0700

    Dung 31 thay doi file thong bao o nhanh main

--- thongbao.cpp
+++ thongbao.cpp
@@ -6,5 +6,7 @@ int main() {
    cout << "Hello World!";
    cout << "TH2: Day la thay doi dau tien";
    cout << "TH2: Day la thay doi thu 2";
+   cout << "TH2: Day la thay doi thu 2";
+   cout << "Dung31 thay doi tren nhanh Main";
     return 0;
 }
