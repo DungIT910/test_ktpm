26e95f2c28152472cf814353164f8a78e760b706
DungIT910 <Dungtb611@gmail.com>
2023-10-23 14:22:47 +0700

    Chinh sua tren Dung31 de tao conflict

--- thongbao.cpp
+++ thongbao.cpp
@@ -5,5 +5,9 @@ using namespace std;
 int main() {
    cout << "Hello World!";
    cout << "TH2: Day la thay doi dau tien";
+   cout << "TH2: Day la thay doi thu 2";
     return 0;
+}
+void func() {
+    cout << "BRANCH DUNG31";
 }
